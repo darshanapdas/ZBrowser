295fa36ab3ab66883f75ea1d7ac969f29b464a84
Tina Johnson <tinajohnson.1234@gmail.com>
2014-08-03 00:29:52 +0530

    Fixed syntax errors in app.js and tested the working of $window

--- zac/js/app.js
+++ zac/js/app.js
@@ -1,21 +1,21 @@
 var zacApp = angular.module('zacApp', ['ngRoute']);
 
-	// configure our routes
+        // configure our routes
 	zacApp.config(function($routeProvider) {
 		$routeProvider
 
-			// route for the home page
+                        // route for the home page
 			.when('/', {
 				templateUrl : 'partials/home.html',
 				controller  : 'mainController'
 			})
 
-			// route for the about page
+                        // route for the about page
 			.when('/birds', {
 				templateUrl : 'partials/birds.html',
 				controller  : 'birdsController'
 			})
-			//route for television
+                        //route for television
 			.when('/television', {
 				templateUrl : 'partials/television.html',
 				controller  : 'televisionController'
@@ -25,31 +25,31 @@ var zacApp = angular.module('zacApp', ['ngRoute']);
 				templateUrl : 'partials/video.html',
 				controller  : 'videoController'
 			})
-			// route for the contact page
+                        // route for the contact page
 			.when('/life', {
 				templateUrl : 'partials/dailyLife.html',
 				controller  : 'dailyLifeController'
-			});
-			//route for books page
+			})
+                        //route for books page
 			.when('/books', {
 				templateUrl : 'partials/books.html',
 				controller  : 'booksController'
-			});
-			//route for games page
+			})
+                        //route for games page
 			.when('/games', {
 				templateUrl : 'partials/games.html',
 				controller  : 'gamesController'
-			});
-			//route for music page
+			})
+                        //route for music page
 			.when('/music', {
 				templateUrl : 'partials/music.html',
 				controller  : 'musicController'
-			});
-			//route for draw page
+			})
+                        //route for draw page
 			.when('/draw', {
 				templateUrl : 'partials/draw.html',
 				controller  : 'drawController'
-			});
+			})
 	});
 
 
--- zac/js/controllers/televisionCtrl.js
+++ zac/js/controllers/televisionCtrl.js
@@ -1,6 +1,8 @@
-
-zacApp.controller('televisionController', function($scope) {
-	$scope.message='Hello televisionController';
+zacApp.controller('televisionController', function($scope,$window) {
+	 $scope.go = function() {
+		$window.location.href='https://google.com';
+	 }
+	 $scope.go();
 	 console.log("television");
 }); 
 
